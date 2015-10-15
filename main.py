b7e047ef6fa2d86bcef1bed106d8ae25922899ab
iwj <jentlewoo@gmail.com>
2015-10-15 18:43:04 +0800

    add web pages

---
+++ main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+
+
+def main():
+    #TODO 获取成员
+    #TODO 根据校历生成值日日历
+    #TODO 按床位号依次安排值日生，每天更新一次
+    pass
+
+
+if __name__ == "__main__":
+    main()
