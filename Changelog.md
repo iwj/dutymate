27bca1639115f85a64d97f1ffa6cbbfe546c1332
iwj <jentlewoo@gmail.com>
2015-10-18 14:14:55 +0800

    update with visual change

--- Changelog.md
+++
@@ -1,4 +0,0 @@
-2015-10-13 J Woo  <jentlewoo@gmail.com> 
-
-    * version 0.1: basic feature
-    display name and number
--- js/index.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	var index = (~~(Date.now()/(60 * 60 * 24 * 1000)))%8;
-	var menbers = $(".members-box>.member");
-	var menber = menbers.eq(index+2);
-	menber.find("a").removeClass("btn-danger").addClass("btn-primary");
-}());
