b976e320fca345b37cd0969359b26641452a51b4
iwj <jentlewoo@gmail.com>
2015-10-18 17:11:45 +0800

    mobile friendly

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
-debugger;
-
 (function() {
 	var index = (~~(Date.now()/(60 * 60 * 24 * 1000))+2)%8;
 	var mates = $(".mate-box>.mate");
 	var duty = mates.eq(index);
 	duty.find("span").addClass("duty-people");
 	console.log(index, duty, mates)
+}());
+
+(function(){
+ 	var date = new Date();
+
+ 	document.getElementById("now-year").innerHTML=date.getFullYear();
+
+	document.getElementById("now-month").innerHTML=date.getMonth()+1;
+
+	document.getElementById("now-day").innerHTML=date.getDate();
+
+	var weekdayChinese = ["日", "一", "二", "三", "四", "五", "六"];
+
+	document.getElementById("now-weekday").innerHTML=weekdayChinese[date.getDay()];
 }());
