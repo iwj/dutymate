0ff643cb22f762bf4cc62aa3ca4928dea3bad9de
iwj <jentlewoo@gmail.com>
2016-03-16 11:23:42 +0800

    update schedule

--- js/tesla.js
+++ js/tesla.js
@@ -8,14 +8,15 @@
 
 (function(){
  	var date = new Date();
-
  	document.getElementById("now-year").innerHTML=date.getFullYear();
-
 	document.getElementById("now-month").innerHTML=date.getMonth()+1;
-
 	document.getElementById("now-day").innerHTML=date.getDate();
-
 	var weekdayChinese = ["日", "一", "二", "三", "四", "五", "六"];
-
 	document.getElementById("now-weekday").innerHTML=weekdayChinese[date.getDay()];
+}());
+
+(function(){
+	// 用作突出显示当前星期day
+	var date = new Date();
+
 }());
