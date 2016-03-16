d648b60f504fd33208724679d7b353087dfe8b8b
iwj <jentlewoo@gmail.com>
2016-03-16 12:15:42 +0800

    add navi into schedule, update js

--- js/tesla.js
+++ js/tesla.js
@@ -3,7 +3,7 @@
 	var mates = $(".mate-box>.mate");
 	var duty = mates.eq(index);
 	duty.find("span").addClass("duty-people");
-	console.log(index, duty, mates)
+	// console.log(index, duty, mates)
 }());
 
 (function(){
@@ -16,7 +16,14 @@
 }());
 
 (function(){
-	// 用作突出显示当前星期day
+	// 用作突出显示当天星期几
 	var date = new Date();
 
+}());
+
+
+(function(){
+	// 显示当前第几周
+	var index = ~~(   (~~(Date.now() / (60 * 60 * 24 * 1000))-3)  /7 )-2407
+	document.getElementById("work-week").innerHTML=index;
 }());
